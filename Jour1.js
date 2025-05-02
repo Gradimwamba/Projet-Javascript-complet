a6fdc34d56329fdeb1d6c859ccbcdff4bebf11ec
gradi <gradimwamba25@gmail>
2025-05-02 21:22:05 +0100

    correction des exercices

--- Jour1.js
+++ Jour1.js
@@ -2,15 +2,18 @@ const monNomComplet = 'Gradi MWAMBA'
 let monAge = 24
 let maVille = 'Kinshasa'
 
-let msg = `je m\'appele ${monNomComplet}, + j\'ai ${monAge} ans + et je vis à ${maVille}.`
+let msg = `je m'appele ${monNomComplet}, j\'ai ${monAge} ans  et je vis à ${maVille}.`
 
 let mesPropietes = ['monNomComplet', 'monAge', 'maVille']
 
- function presentation(mesPropietes) {
+ function presentation(monNomComplet,monAge) {
     return msg
 }
 console.log(presentation(mesPropietes)); 
 
+
+const presentention = () => `je m'appele ${monNomComplet},  j'ai ${monAge} ans  et je vis à ${maVille}.`
+console.log(presentention()); //Affiche 'je m'appelle Gradi MWAMBA, j'ai 24 ans et je vis à Kinshasa'
 
 
 let myTable = ['JavaScript', 'Python', 'Java', 'C#', 'PHP',]
@@ -24,5 +27,14 @@ let profil = {
     langages: 'Lingala'
 }
 
-// const presentation (monAge) => 'je m\'appele + ${monNomComplet}'
-// console.log(msg);
+
+const PrenomComplet = 'Exauce MBOMA'
+let commune = 'Kalamu'
+let quartier = 'Kauka'
+let age = 30
+
+let messages = `Je préfère me présenter, je suis ${PrenomComplet}, j'ai ${age} ans, j'habite dans la commune de ${commune} au quartier ${quartier}`
+
+const identité = (commune,quartier, age) => messages
+
+console.log(identité (messages));
