4a52345d20f3b72564f293e752ac00af2db8c797
gradi <gradimwamba25@gmail>
2025-05-02 20:13:54 +0100

    Premier commit projet Js jour1

--- Jour1.js
+++ Jour1.js
@@ -2,12 +2,27 @@ const monNomComplet = 'Gradi MWAMBA'
 let monAge = 24
 let maVille = 'Kinshasa'
 
-const presentation(moncordonée) => `je m\'appele ${monNomComplet}, + j\'ai ${monAge} ans + et je vis ${maVille}.`
-console.log(moncordonée);
+let msg = `je m\'appele ${monNomComplet}, + j\'ai ${monAge} ans + et je vis à ${maVille}.`
+
+let mesPropietes = ['monNomComplet', 'monAge', 'maVille']
+
+ function presentation(mesPropietes) {
+    return msg
+}
+console.log(presentation(mesPropietes)); 
+
+
+
+let myTable = ['JavaScript', 'Python', 'Java', 'C#', 'PHP',]
+console.log(myTable);
+
 
 let profil = {
     Nom : '',
     age : 26,
-   ville : 'Kinshasa',
-   langages: 'Lingala'
+    ville : 'Kinshasa',
+    langages: 'Lingala'
 }
+
+// const presentation (monAge) => 'je m\'appele + ${monNomComplet}'
+// console.log(msg);
